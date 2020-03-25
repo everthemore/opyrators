02f84a07f5b971ca42622dca31fd0c251611fcc0
Evert van Nieuwenburg <evert.v.nieuwenburg@gmail.com>
2020-03-25 13:31:05 +0100

    Update

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="opyrators",
-    version="0.0.4",
+    version="0.0.1",
     author="Evert van Nieuwenburg",
     author_email="evert.v.nieuwenburg@gmail.com",
-    description="Compute commutators of quantum operators",
+    description="Manipulate quantum many-body operators",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/everthemore/opyrators",
@@ -18,7 +18,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
-        "Development Status :: 4 -- Beta",
+        "Development Status :: 3 -- Alpha",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
